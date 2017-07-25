4c87ee03e633016de643796347cfa53a5d005ad7
Ethan19 <flymazda@163.com>
2017-07-26 01:15:21 +0800

    第二题

--- 1.php
+++ 1.php
@@ -1,3 +1,23 @@
 <?php
+$file = "data";
+$fp = fopen($file, "r");
+$str  = '';
+while(!feof($fp))
+{
+	$str = fgets($fp);
+	if(strlen($str)>=13)
+	{
+		file_put_contents("databak", $str,FILE_APPEND|LOCK_EX);
+	}
+}
 
-echo 111;:
+fclose($fp);
+$memoryLevel = memory_get_peak_usage(false);
+echo ($memoryLevel/1024/1024)."m\n";
+// echo "<br/>";
+// var_dump($res/1024);
+// die;
+        
+
+
+
